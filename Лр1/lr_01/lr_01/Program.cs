b105fe9505356a280449f9837f393a0ec6dd6434
Iryna <joker.1.666@mail.ru>
2022-09-09 13:58:11 +0300

    #1

---
+++ Лр1/lr_01/lr_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lr_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool BooleanVariable = true;
+            Console.WriteLine("Boolean Variable: ");
+            byte ByteVariable;
+            Console.WriteLine("Hello World!");
+            sbyte SbyteVariable;
+            
+            char CharVariable;
+            
+            decimal DecimalVariable;
+            
+            double DoubleVariable;
+            
+            float FloatVariable;
+            
+            int IntVariable;
+            
+            uint UintVariable;
+            
+            long LongVariable;
+            
+            ulong UlongVariable;
+            
+            short ShortVariable;
+            
+            ushort UshortVariable;
+        }
+    }
+}

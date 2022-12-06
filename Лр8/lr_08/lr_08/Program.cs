376ffa05f8829d733ef232d981a740e2c507af74
Iryna <joker.1.666@mail.ru>
2022-12-06 13:52:59 +0300

    delegate, events: Move, Compress, class User, lambda, objects

---
+++ Лр8/lr_08/lr_08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static lr_08.User;
+
+namespace lr_08
+{
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User(155);
+            User user2 = new User("disk C");
+            User user3 = new User(255);
+            User user4 = new User("disk E");
+
+            user1.Compression(200);
+            Console.WriteLine($"File weight {user1.Weight}");
+            user2.Movement("dick D");
+            Console.WriteLine($"Users file was moved to {user2.Location}");
+
+            user3.Compression(743);
+            Console.WriteLine($"File weight {user3.Weight}");
+            user4.Movement("recycle bin");
+            Console.WriteLine($"Users file was moved to {user4.Location}");
+
+            //user1.Compress += Compression;
+        }
+    }
+}
---
+++ Лр8/lr_08/lr_08/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_08
+{
+    public class User
+    {
+        //UserMessage userMessage = Add;
+        /*void Add(int x) => Console.WriteLine("w");*/
+        public User(int weight) => Weight = weight;
+        public User(string location) => Location = location;
+        public int Weight { get; set; }
+        public string Location { get; set; }
+
+        public void Compression(int weight)
+        {
+            Weight = weight / 2;
+            Compress?.Invoke($"Compressed weight of file is {Weight}");
+        }
+
+        public void Movement(string location)
+        {
+            Location = location;
+            Move?.Invoke($"New file location is {Location}");
+        }
+
+       /* public delegate void Message();
+        
+        public void MessageofCompression() => Console.WriteLine("Compression completed");
+        public void MessegeofMovement() => Console.WriteLine("Movement completed");
+
+        Message message = MessageofCompression;
+        message();*/
+
+        //public void Compression(int weight) => Weight = weight / 2; 
+
+        public delegate void UserMessage(string message);
+        event UserMessage? Move;
+        public event UserMessage? Compress;
+    }
+}

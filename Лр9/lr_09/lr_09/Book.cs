64b470898b5e1ef1f3be3e42db0a4caf5643137c
Iryna <joker.1.666@mail.ru>
2022-12-13 15:45:47 +0300

    class Book, interface IDictionary, collection List, methods

---
+++ Лр9/lr_09/lr_09/Book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_09
+{
+    interface IDictionary
+    {
+        public void Add(Book key);
+        public void Remove(Book key);
+    }
+    public class Book 
+    {  
+        public Book()
+        { }
+        
+        public string Name { get; set; }
+        public Book(string name) => Name = name;
+    }
+
+    public class CollectionList : IDictionary
+    {
+        List<Book> books = new List<Book>();
+        public void Add(Book key)
+        {
+            books.Add(key);
+        }
+
+        public void Remove(Book key)
+        {
+            books.Remove(key);
+        }
+
+        public void Search(Book key)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (key == books[i])
+                {
+                    Console.WriteLine("Element: " + books[i]);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No such object in list");
+                    break;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                Console.WriteLine(books[i]);
+            }
+        }
+    }
+}
---
+++ Лр9/lr_09/lr_09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lr_09
+{
+    public class Programm
+    {
+        static void Main(string[] args)
+        { 
+            CollectionList list = new CollectionList();
+            Book book1 = new Book();
+            Book book2 = new Book();
+            Book book3 = new Book();
+
+
+            list.Add(book1);
+            list.Add(book2);
+            list.Add(book3);
+            //list.Print();
+            list.Remove(book2);
+            list.Search(book1);
+            list.Search(book2);
+            list.Print();
+        }
+    }
+}

45f266985bea2105c1cc0ca7393221af1b4b2f82
Iryna <joker.1.666@mail.ru>
2022-12-25 06:27:03 +0300

    list, queries

---
+++ Лр10/lr_10/lr_10/Airline.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_10
+{
+    public class Airline
+    {
+        public string destination;
+        public int flight_number;
+        public string plane_type;
+        public int leave_time;
+        public string week_day;
+        public string[] week = new string[7] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sutarday", "Sunday" };
+        static int flight_num;
+        static string type;
+        public static int Flight_num => flight_num;
+        public static string Type => type;
+        public readonly string place = "Riga";
+        public readonly string IdDestination;
+        public const string PLACE = "Minsk";
+        static int count = 0;
+
+        public Airline() // 1
+        {
+            count++;
+            int day = 5;
+            for (int i = 0; i < 7; i++)
+            {
+                if (day > 7)
+                {
+                    Console.WriteLine("Uncorrect value, try again");
+                    Console.WriteLine("Enter day number on week: ");
+                    day = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (day == i)
+                {
+                    week_day = week[i - 1];
+                    break;
+                }
+
+            }
+
+            destination = "Madrid";
+            flight_number = 455;
+            plane_type = "Ty-134";
+            leave_time = 18;
+        }
+
+        public Airline(int day, string location, int number, int time, string type) // 2
+        {
+            count++;
+            for (int i = 0; i < 7; i++)
+            {
+                if (day > 7)
+                {
+                    Console.WriteLine("Uncorrect value, try again");
+                    Console.WriteLine("Enter day number on week: ");
+                    day = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (day == i)
+                {
+                    week_day = week[i - 1];
+                    break;
+                }
+
+            }
+            destination = location;
+            plane_type = type;
+            flight_number = number;
+            leave_time = time;
+        }
+
+        public Airline(string type, string location = "London", int day = 7, int number = 457, int time = 9) // 3 
+        {
+            // string destination, int flight_number = 457, string plane_type = "Ty-134"
+            count++;
+            destination = location;
+            plane_type = type;
+            for (int i = 0; i < 8; i++)
+            {
+                if (day > 7)
+                {
+                    Console.WriteLine("Uncorrect value, try again");
+                    Console.WriteLine("Enter day number on week: ");
+                    day = Convert.ToInt32(Console.ReadLine());
+                }
+
+                if (day == i)
+                {
+                    week_day = week[i - 1];
+                    break;
+                }
+
+            }
+            flight_number = number;
+            ref int NumPointer = ref flight_number;
+            leave_time = time;
+        }
+
+        public Airline(out int result, ref int num, string location)
+        {
+            result = num + 1;
+        }
+        static Airline()
+        {
+            flight_num = 457;
+            if (flight_num == 457)
+            {
+                type = "Il-86";
+            }
+            else
+                type = "Ty-134";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Destination: {destination}, flight number: {flight_number}, type of plane - {plane_type}, day {week_day}, leave time {leave_time}");
+            Console.WriteLine("Counter: " + count);
+        }
+    }
+}
--- Лр10/lr_10/lr_10/Program.cs
+++ Лр10/lr_10/lr_10/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace lr_10
 {
@@ -52,6 +61,68 @@ namespace lr_10
             }
             Console.WriteLine();
             Console.WriteLine("-----------------------");
+
+            // ========================================
+            Airline airline = new Airline(2, "Berlin", 2, 2, "Ty-134");
+            Airline airline1 = new Airline(4, "London", 4, 19, "Ty-134");
+            Airline airline2 = new Airline(3, "Stokholm", 3, 22, "Ty-134");
+            Airline airline3 = new Airline(1, "Luxemburg", 1, 17, "Ty-134");
+            Airline airline4 = new Airline(7, "Oslo", 7, 6, "Ty-134");
+            Airline airline5 = new Airline(7, "Paris", 7, 10, "Ty-134");
+            Airline airline6 = new Airline(6, "Praga", 6, 11, "Ty-134");
+            Airline airline7 = new Airline(3, "Madrid", 3, 15, "Ty-134");
+            Airline airline8 = new Airline(3, "Gamburg", 5, 2, "Ty-134");
+            Airline airline9 = new Airline(5, "Leipzig", 5, 19, "Ty-134");
+
+            List<Airline> airlines = new List<Airline>();
+
+            airlines.Add(airline);
+            airlines.Add(airline1);
+            airlines.Add(airline2);
+            airlines.Add(airline3);
+            airlines.Add(airline4);
+            airlines.Add(airline5);
+            airlines.Add(airline6);
+            airlines.Add(airline7);
+            airlines.Add(airline8);
+            airlines.Add(airline9);
+
+            var selectflight = airlines.Where(a => a.destination.Contains("Leipzig"));
+            foreach (var flight in selectflight)
+            {
+                Console.Write(flight.destination + " - " + flight.week_day + " " + flight.leave_time);
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+
+            var selectflight1 = from a in airlines where a.week_day == "Monday" select a;
+            var selectflight4 = from a in airlines where a.week_day == "Wednesday" || a.week_day == "Friday" select a;
+            var selectflight2 = from f in selectflight1 where f.leave_time < 20 select f;
+            var selectflight3 = from f in selectflight4 where f.leave_time > 17 select f;
+            foreach (var flight in selectflight2)
+            {
+                Console.Write(flight.destination + " - " + flight.week_day + " " + flight.leave_time);
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            foreach (var flight in selectflight3)
+            {
+                Console.Write(flight.destination + " - " + flight.week_day + " " + flight.leave_time + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+
+            var orderbytime = airlines.OrderBy(a => a.leave_time);
+            foreach (var flight in orderbytime)
+            {
+                Console.Write(flight.destination + " - " + flight.week_day + " " + flight.leave_time + "; ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            /*foreach (var elem in airlines)
+            {
+                Console.WriteLine(elem);
+            }*/
         }
     }
 }

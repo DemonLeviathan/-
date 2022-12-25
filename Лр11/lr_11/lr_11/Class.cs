9b643eed70eacf37e3bef5c68175d86dcdff54ac
Iryna <joker.1.666@mail.ru>
2022-12-26 02:38:15 +0300

    Reflection for classes: Book, User, System.Object

---
+++ Лр11/lr_11/lr_11/Class.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_11
+{
+    public class User
+    {
+        public User(int weight) => Weight = weight;
+        public User(string location) => Location = location;
+        public int Weight { get; set; }
+        public string Location { get; set; }
+
+        public void Compression(int weight)
+        {
+            Weight = weight / 2;
+            Compress?.Invoke($"Compressed weight of file is {Weight}");
+        }
+
+        public void Movement(string location)
+        {
+            Location = location;
+            Move?.Invoke($"New file location is {Location}");
+        }
+
+        public delegate void UserMessage(string message);
+        event UserMessage? Move;
+        public event UserMessage? Compress;
+
+        string result = "";
+    }
+
+    public class Book
+    {
+        public Book()
+        { }
+
+        public string Name { get; set; }
+        public Book(string name) => Name = name;
+    }
+}
--- Лр11/lr_11/lr_11/Program.cs
+++ Лр11/lr_11/lr_11/Program.cs
@@ -10,6 +10,8 @@ namespace lr_11
         static void Main(string[] args)
         {
             Programm programm = new Programm();
+            User user = new User(17);
+            Book book = new Book();
             Console.WriteLine("Name of assembly");
             Reflector.GetName("lr_11.Person");
             Console.WriteLine("------------------------");
@@ -32,8 +34,81 @@ namespace lr_11
 
             Console.WriteLine("Calling class method by invoke: ");
             Reflector.PrintMethod("lr_11.Person", "String");
+            Console.WriteLine("------------------------------------------------------");
+
+
+            Console.WriteLine("Name of assembly");
+            Reflector.GetName("lr_11.User");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Is any constructors?: ");
+            Reflector.IsConstructurs("lr_11.User");
             Console.WriteLine("------------------------");
 
+            Console.WriteLine("All public methods: ");
+            Reflector.GetMethod("lr_11.User");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Information about fields and class properties: ");
+            Reflector.GetField("lr_11.User");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Interfaces: ");
+            Reflector.GetInterface("lr_11.User");
+            Console.WriteLine("------------------------");
+
+
+            Console.WriteLine("Calling class method by invoke: ");
+            Reflector.PrintMethod("lr_11.User", "String");
+            Console.WriteLine("-----------------------------------------------------");
+
+
+            Console.WriteLine("Name of assembly");
+            Reflector.GetName("lr_11.Book");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Is any constructors?: ");
+            Reflector.IsConstructurs("lr_11.Book");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("All public methods: ");
+            Reflector.GetMethod("lr_11.Book");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Information about fields and class properties: ");
+            Reflector.GetField("lr_11.Book");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Interfaces: ");
+            Reflector.GetInterface("lr_11.Book");
+            Console.WriteLine("------------------------");
+
+
+            Console.WriteLine("Calling class method by invoke: ");
+            Reflector.PrintMethod("lr_11.Book", "String");
+            Console.WriteLine("-----------------------------------------------");
+
+            //Console.WriteLine("Name of assembly");
+            //Reflector.GetName("lr_11.Object");
+            //Console.WriteLine("------------------------");
+            Console.WriteLine("Is any constructors?: ");
+            Reflector.IsConstructurs("System.Object");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("All public methods: ");
+            Reflector.GetMethod("System.Object");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Information about fields and class properties: ");
+            Reflector.GetField("System.Object");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Interfaces: ");
+            Reflector.GetInterface("System.Object");
+            Console.WriteLine("------------------------");
+
+
+            Console.WriteLine("Calling class method by invoke: ");
+            Reflector.PrintMethod("System.Object", "String");
+            Console.WriteLine("-----------------------------------------------");
         }
     }
 }

183f237b40ed98fe8cc43d4bf72497408ec5da6a
Iryna <joker.1.666@mail.ru>
2022-12-25 03:29:17 +0300

    universal collection, observablecollection,  event collectionchange

--- Лр9/lr_09/lr_09/Book.cs
+++ Лр9/lr_09/lr_09/Book.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace lr_09
 {
@@ -23,14 +24,31 @@ namespace lr_09
     public class CollectionList : IDictionary
     {
         List<Book> books = new List<Book>();
+        List<int> bookID = new List<int>();
+        HashSet<Book> bookSet = new HashSet<Book>();
         public void Add(Book key)
         {
             books.Add(key);
         }
 
+        public void AddToSet(Book key)
+        {
+            bookSet.Add(key);
+        }
+
+        public void AddToInt(int i)
+        {
+            bookID.Add(i);
+        }
+
         public void Remove(Book key)
         {
             books.Remove(key);
+        }
+
+        public void RemoveFromInt()
+        {
+            bookID.RemoveRange(1, 3);
         }
 
         public void Search(Book key)
@@ -50,6 +68,16 @@ namespace lr_09
             }
         }
 
+        public void SearchInSet(Book key)
+        {
+            bool found = false;
+            for (int i = 0; i < bookSet.Count; i++)
+            {
+                found = bookSet.TryGetValue(key, out Book book);
+            }
+            Console.WriteLine("Is element in hashset? : {0}", found);
+        }
+
         public void Print()
         {
             for (int i = 0; i < books.Count; i++)
@@ -57,5 +85,20 @@ namespace lr_09
                 Console.WriteLine(books[i]);
             }
         }
+
+        public void PrintInt()
+        {
+            for (int i =0; i < bookID.Count; i++)
+            {
+                Console.WriteLine(bookID[i]);
+            }
+        }
+
+        public void PrintSet()
+        {
+            Console.WriteLine(bookSet);
+        }
+
     }
+
 }
--- Лр9/lr_09/lr_09/Program.cs
+++ Лр9/lr_09/lr_09/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
 
 namespace lr_09
 {
@@ -7,6 +10,7 @@ namespace lr_09
         static void Main(string[] args)
         { 
             CollectionList list = new CollectionList();
+            //CollectionList collectionList = new CollectionList();
             Book book1 = new Book();
             Book book2 = new Book();
             Book book3 = new Book();
@@ -20,6 +24,46 @@ namespace lr_09
             list.Search(book1);
             list.Search(book2);
             list.Print();
+            Console.WriteLine("--------------------------");
+
+            list.AddToInt(11);
+            list.AddToInt(13);
+            list.AddToInt(15);
+            list.AddToInt(17);
+            list.AddToInt(19);
+            list.AddToInt(23);
+            list.PrintInt();
+            Console.WriteLine("--------------------------");
+            list.RemoveFromInt();
+            list.PrintInt();
+
+            Console.WriteLine("--------------------------");
+
+            list.AddToSet(book1);
+            list.AddToSet(book2);
+            list.AddToSet(book3);
+            list.SearchInSet(book3);
+            list.PrintSet();
+            Console.WriteLine("--------------------------");
+
+            ObservableCollection<Book> strings = new ObservableCollection<Book>();
+            strings.CollectionChanged += CollectionChange;
+            strings.Add(book1);
+            strings.Remove(book1);
+            //book1.Change(book3);
+        }
+        private static void CollectionChange(object obj, NotifyCollectionChangedEventArgs args)
+        {
+            switch (args.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Console.WriteLine($"New element was added");
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    Console.WriteLine($"Element was removed");
+                    break;
+            }
         }
     }
 }

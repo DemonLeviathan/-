35f718d48029869bcba2943569553dc118ff2444
Iryna <joker.1.666@mail.ru>
2022-12-27 12:38:26 +0300

    class VISLog, vislogfile.txt, methods for writing, reading, search

---
+++ Лр12/lr_12/lr_12/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_12
+{
+    public class VISLog
+    {
+        string path = @"D:\Универ\3-й семестр\Лабы ООП\Лр12\lr_12\lr_12\vislogfile.txt";
+        public void Write(string text)
+        {
+            using (StreamWriter fstream = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                fstream.WriteLine("File name: {0}", Path.GetFileName(path));
+                fstream.WriteLine("File location: {0}", path);
+                fstream.WriteLine("Date: {0}", DateTime.Now);
+                fstream.WriteLine(text);
+                fstream.WriteLine("--------------------------");
+            }
+        }
+
+        public string Read()
+        {
+            string path = @"D:\Универ\3-й семестр\Лабы ООП\Лр12\lr_12\lr_12\vislogfile.txt";
+            StreamReader reader = new StreamReader(path);
+            return reader.ReadToEnd();
+        }
+
+        public void Search(string text)
+        {
+            string path = @"D:\Универ\3-й семестр\Лабы ООП\Лр12\lr_12\lr_12\vislogfile.txt";
+            string[] findInfo = File.ReadAllLines(path);
+
+            for(int i = 0; i < findInfo.Length; i++)
+            {
+                if (findInfo[i].Contains(text))
+                {
+                    Console.WriteLine("{0} is in file", text);
+                }
+                else
+                    Console.WriteLine("{0} isn't in file", text);
+            }
+        }
+    }
+}
---
+++ Лр12/lr_12/lr_12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace lr_12
+{
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            string path = "vislogfile.txt";
+
+            File.WriteAllText(path, "-----Logging-------\n");
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            
+
+            /*DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем диска: {drive.TotalSize}");
+                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка диска: {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }*/
+
+        }
+    }
+}

e14d81f9304ecc84b1e76dbf0f4e021c6485ba58
Iryna <joker.1.666@mail.ru>
2023-01-11 19:19:16 +0300

    continuation task, class parallel, parallel invoke

--- Лр15/lr_15/lr_15/Program.cs
+++ Лр15/lr_15/lr_15/Program.cs
@@ -47,6 +47,77 @@ namespace lr_15
             Task<int> result = new Task<int>(() => Average(rand1.Result, rand2.Result, rand3.Result));
             result.Start();
             Console.WriteLine($"Result value: {result.Result}");
+
+            Console.WriteLine("-------------------------------------------");
+
+            Task<int> con1 = new Task<int>(Sum);
+            Task<int> con2 = con1.ContinueWith((task) => Mult());
+            Task<int> con3 = con2.ContinueWith((task) => Diff());
+            Task<int> con4 = con3.ContinueWith((task) => Average(con1.Result, con2.Result, con3.Result));
+            con1.Start();
+
+            Console.WriteLine($"First value: {con1.Result}");
+            Console.WriteLine($"Second value: {con2.Result}");
+            Console.WriteLine($"Third value: {con3.Result}");
+            Console.WriteLine($"Result value: {con4.Result}");
+
+            con1.Dispose();
+            con2?.Dispose();
+            con3?.Dispose();
+            con4.Dispose();
+
+            Task<int> wait = Task.Run(() => Enumerable.Range(1, 10000).Count(n => (n % 3 == 0)));
+            TaskAwaiter<int> awaiter = wait.GetAwaiter();
+            awaiter.OnCompleted(() => { int result = awaiter.GetResult();
+                Console.WriteLine(result);
+            });
+            Console.WriteLine(awaiter.GetResult());
+            Console.WriteLine("-------------------------------------------");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            int[] ints1 = new int[1000000];
+            int[] ints2 = new int[1000000];
+            Random random = new Random();
+            sw.Restart();
+
+            for (int i = 0; i < ints1.Length; i++)
+            {
+                ints1[i] = random.Next(0, 100);
+                ints2[i] = random.Next(0, 100);
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Time of compliting array through for: {sw.Elapsed}");
+
+            sw.Restart();
+
+            Parallel.ForEach<int>(ints1, (i) =>
+            {
+                ints1[i] = random.Next(0, 100);
+                ints2[i] = random.Next(0, 100);
+            });
+            Console.WriteLine($"Time of compliting array through foreach: {sw.Elapsed}");
+
+            Parallel.Invoke(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Parallel compliting of block 1: {i}");
+                }
+                Console.WriteLine("Block 1 in ready");
+            },
+            () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Parallel compliting of block 2: {i}");
+                }
+                Console.WriteLine("Block 2 in ready");
+
+            });
+            Console.WriteLine("-------------------------------------------");
+
+
         }
     }
 }
